307fb90e0efae0aea3203006f271015ddf5cc27d
marek0209 <marek.urbaniec1@gmail.com>
2023-04-02 01:11:43 +0200

    Add HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1> Dashboard</h1>HomePage
+    </>
+  );
+};
+
+export default HomePage;
