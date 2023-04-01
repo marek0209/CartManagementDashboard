82a0a4ad9b2daea2f79291cafcc3f5521ce4f89f
marek0209 <marek.urbaniec1@gmail.com>
2023-04-02 00:50:20 +0200

    Add tests for fetching single cart

--- src/services/cartService.test.ts
+++ src/services/cartService.test.ts
@@ -1,4 +1,4 @@
-import { getCarts } from "./cartService";
+import { getCarts, getSingleCart } from "./cartService";
 
 describe("getCarts", () => {
   it("should return an array of carts", async () => {
@@ -14,5 +14,26 @@ describe("getCarts", () => {
       expect(cart.products).toBeDefined();
       expect(cart.products[0].id).toBeDefined();
     });
+  });
+});
+
+describe("getSingleCart", () => {
+  it("should return a single cart with expected properties", async () => {
+    const cartId = 1;
+    const cart = await getSingleCart(cartId);
+    expect(cart.id).toBe(cartId);
+    expect(cart.userId).toBeDefined();
+    expect(cart.products).toBeDefined();
+    expect(cart.total).toBeDefined();
+    expect(cart.totalProducts).toBeDefined();
+    expect(cart.totalQuantity).toBeDefined();
+    expect(cart.discountedTotal).toBeDefined();
+  });
+
+  it("should throw an error for non-existent cart", async () => {
+    const cartId = 999;
+    await expect(getSingleCart(cartId)).rejects.toThrow(
+      "The requested resource was not found."
+    );
   });
 });
