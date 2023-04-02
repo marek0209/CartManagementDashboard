2e73797a2b06643d8798fd305db247423462feeb
marek0209 <marek.urbaniec1@gmail.com>
2023-04-02 02:57:47 +0200

    Change routing for CartDetailsPage

--- src/pages/CartDetailsPage/CartDetailsPage.tsx
+++ src/pages/CartDetailsPage/CartDetailsPage.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import CartItems from "../../components/CartItems/CartItems";
+import { useParams } from "react-router-dom";
 
-const CartDetailsPage: React.FC = () => <CartItems id="1" />;
+const CartDetailsPage: React.FC = () => {
+  const { id } = useParams<string>();
+  return <CartItems id={id} />;
+};
 
 export default CartDetailsPage;
