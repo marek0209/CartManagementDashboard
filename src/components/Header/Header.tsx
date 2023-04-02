63f1ea65e2b99d90924b7d1f7c86bb99dd020344
marek0209 <marek.urbaniec1@gmail.com>
2023-04-02 22:44:44 +0200

    Change Header Styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <>
-            <div className={styles.header}></div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.header}></div>
+    </>
+  );
 };
 
 export default Header;
