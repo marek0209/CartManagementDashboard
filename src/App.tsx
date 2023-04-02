078c2edf7af36f2179a4721d39cdbb179d29afd4
marek0209 <marek.urbaniec1@gmail.com>
2023-04-02 12:55:32 +0200

    Fix typo in path

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/carts" element={<CartsPage />} />
-          <Route path="/cart/:id" element={<CartDetailsPage />} />
+          <Route path="/carts/:id" element={<CartDetailsPage />} />
         </Routes>
       </Router>
     </>
