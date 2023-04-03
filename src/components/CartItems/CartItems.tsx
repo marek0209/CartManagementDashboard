3afee5b150e3b6e87750ae1a8e1ec8f7520d87d1
marek0209 <marek.urbaniec1@gmail.com>
2023-04-03 02:17:56 +0200

    Move logic from component to Page

--- src/components/CartItems/CartItems.tsx
+++ src/components/CartItems/CartItems.tsx
@@ -1,35 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CartItemDetails } from "../../types/cartTypes";
-import { getSingleCart } from "../../services/cartService";
-import styles from "./CartItems.module.css";
 import Error from "../Error/Error";
-import PriceChart from "../PriceChart/PriceChart";
+import styles from "./CartItems.module.css";
 
 interface Props {
   id: string | undefined;
+  carts: CartItemDetails[];
+  error: boolean;
 }
 
-const CartItems: React.FC<Props> = ({ id }) => {
-  const [cartItems, setCartItems] = useState<CartItemDetails[]>([]);
-  const [error, setError] = useState(false);
+const CartItems: React.FC<Props> = ({ id, carts, error }) => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    async function fetchCartItems() {
-      try {
-        if (id !== undefined) {
-          const data = await getSingleCart(parseInt(id));
-          setCartItems(data.products);
-        }
-      } catch (error) {
-        setError(true);
-        navigate("/");
-      }
-    }
-
-    fetchCartItems();
-  }, [id, navigate]);
 
   const handleCloseDetails = () => {
     navigate("/");
@@ -42,32 +24,34 @@ const CartItems: React.FC<Props> = ({ id }) => {
   }
 
   return (
-    <div className="cart-details">
-      <h2>Cart Details {id}</h2>
-      <button onClick={handleCloseDetails}>Close</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th>Discount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <tr key={item.id}>
-              <td>{item.title}</td>
-              <td>{item.price}$</td>
-              <td>{item.quantity}</td>
-              <td>{item.total}$</td>
-              <td>{item.discountedPrice}$</td>
+    <div className={styles.CartItemsTableContainer}>
+      <button className={styles.CloseButton} onClick={handleCloseDetails}>
+        Close
+      </button>
+      <div className={styles.TableContainer}>
+        <table className={styles.styledTable}>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              <th>Discount</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <PriceChart cartItems={cartItems} />
+          </thead>
+          <tbody>
+            {carts.map((item) => (
+              <tr key={item.id}>
+                <td>{item.title}</td>
+                <td>{item.price}$</td>
+                <td>{item.quantity}</td>
+                <td>{item.total}$</td>
+                <td>{item.discountedPrice}$</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/pages/CartDetailsPage/CartDetailsPage.tsx
+++ src/pages/CartDetailsPage/CartDetailsPage.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CartItems from "../../components/CartItems/CartItems";
 import { useParams } from "react-router-dom";
+import { getSingleCart } from "../../services/cartService";
+import { CartItemDetails } from "../../types/cartTypes";
+import PriceChart from "../../components/PriceChart/PriceChart";
+
+import styles from "./CartDeatilsPage.module.css";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 const CartDetailsPage: React.FC = () => {
+  const [cartItems, setCartItems] = useState<CartItemDetails[]>([]);
+  const [error, setError] = useState(false);
   const { id } = useParams<string>();
-  return <CartItems id={id} />;
+  useEffect(() => {
+    async function fetchCartItems() {
+      try {
+        if (id !== undefined) {
+          const data = await getSingleCart(parseInt(id));
+          setCartItems(data.products);
+        }
+      } catch (error) {
+        setError(true);
+      }
+    }
+
+    fetchCartItems();
+  }, [id]);
+
+  return (
+    <div className={styles.CartDetailsPageContainer}>
+      <Header />
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <h2>Details of Cart {id}</h2>{" "}
+        <CartItems id={id} carts={cartItems} error={error} />
+        <h3>Price Chart</h3>
+        <PriceChart cartItems={cartItems} />
+      </div>
+
+    </div>
+  );
 };
 
 export default CartDetailsPage;
